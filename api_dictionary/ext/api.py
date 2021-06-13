d582487bed57056f3b62c9f50f8d414c26c6f8a6
utrape <pablosud025@gmail.com>
2021-06-13 19:21:32 -0300

    Add api files

---
+++ api_dictionary/ext/api.py
@@ -0,0 +1,51 @@
+from api_dictionary.ext.webscrapy import dictionary
+from flask import Flask, jsonify
+
+from .webscrapy import English, Portuguese, Spanish
+
+def init_app(app: Flask):
+    
+    @app.route('/v1/en/<word>/')
+    def api_en(word):
+        dictionary = English()
+        if meanings:=dictionary.return_meaning(word):
+            api_return = {
+                'source': dictionary.URL.format(word),
+                'meanings': meanings
+                }
+            code_status = 200
+        else:
+            api_return = {'message': 'Not Found'}
+            code_status = 404
+            print(meanings)
+        return jsonify(api_return), code_status
+    
+    @app.route('/v1/pt/<word>/')
+    def api_pt(word):
+        dictionary = Portuguese()
+        if meanings:=dictionary.return_meaning(word):
+            api_return = {
+                'source': dictionary.URL.format(word),
+                'meanings': meanings
+                }
+            code_status = 200
+        else:
+            api_return = {'message': 'Not Found'}
+            code_status = 404
+            print(meanings)
+        return jsonify(api_return), code_status
+    
+    @app.route('/v1/es/<word>/')
+    def api_es(word):
+        dictionary = Spanish()
+        if meanings:=dictionary.return_meaning(word):
+            api_return = {
+                'source': dictionary.URL.format(word),
+                'meanings': meanings
+                }
+            code_status = 200
+        else:
+            api_return = {'message': 'Not Found'}
+            code_status = 404
+            print(meanings)
+        return jsonify(api_return), code_status

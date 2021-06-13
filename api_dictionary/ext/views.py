e9fd40004a8022cc6ef80186bfbf512757a8f128
Pablo Emidio <p.emidiodev@gmail.com>
2021-06-13 20:13:54 -0300

    Add second index route

--- api_dictionary/ext/views.py
+++ api_dictionary/ext/views.py
@@ -3,5 +3,6 @@ from flask import Flask, render_template
 def init_app(app: Flask)->None:
     
     @app.route('/')
+    @app.route('/index.html/')
     def index():
         return render_template('index.html')

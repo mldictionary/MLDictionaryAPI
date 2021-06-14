ec49569174287995b68b5767aaa454be9d91e51f
utrape <pablosud025@gmail.com>
2021-06-14 09:39:36 -0300

    Reframe api

--- api_dictionary/ext/api.py
+++ api_dictionary/ext/api.py
@@ -1,12 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
-from .webscrapy import English, Portuguese, Spanish
+from .webscrapy import English, Portuguese, Spanish, Translator
 
 def init_app(app: Flask):
     
-    @app.route('/v1/en/<word>/')
-    def api_en(word: str):
-        dictionary = English()
+    @app.route('/v1/dictionary/en/<word>/', methods=['get'])
+    @app.route('/v1/dictionary/pt/<word>/', methods=['get'])
+    @app.route('/v1/dictionary/es/<word>/', methods=['get'])
+    @app.route('/v1/translator/en-pt/<word>/', methods=['get'])
+    def api(word: str):
+        current_route_work = request.url.split('/')[5]
+        route_dictionary = {
+            'en': English(),
+            'pt': Portuguese(),
+            'es': Spanish(),
+            'en-pt': Translator()          
+        }
+        dictionary = route_dictionary[current_route_work]
+
         if meanings:=dictionary.return_meaning(word):
             api_return = {
                 'source': dictionary.URL.format(word),
@@ -15,33 +26,5 @@ def init_app(app: Flask):
             code_status = 200
         else:
             api_return = {'message': 'Not Found'}
-            code_status = 404
-        return jsonify(api_return), code_status
-    
-    @app.route('/v1/pt/<word>/')
-    def api_pt(word):
-        dictionary = Portuguese()
-        if meanings:=dictionary.return_meaning(word):
-            api_return = {
-                'source': dictionary.URL.format(word),
-                'meanings': meanings
-                }
-            code_status = 200
-        else:
-            api_return = {'message': 'Not Found'}
-            code_status = 404
-        return jsonify(api_return), code_status
-    
-    @app.route('/v1/es/<word>/')
-    def api_es(word):
-        dictionary = Spanish()
-        if meanings:=dictionary.return_meaning(word):
-            api_return = {
-                'source': dictionary.URL.format(word),
-                'meanings': meanings
-                }
-            code_status = 200
-        else:
-            api_return = {'message': 'Not Found'}
-            code_status = 404
+            code_status = 404     
         return jsonify(api_return), code_status

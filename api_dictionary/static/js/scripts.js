92f55749bdaa931a7c4915d5908291c9ac20d6f6
Pablo Emidio <p.emidiodev@gmail.com>
2021-06-15 13:21:55 -0300

    Add new responsibilities to scripts.js

--- api_dictionary/static/js/scripts.js
+++ api_dictionary/static/js/scripts.js
@@ -1,5 +1,7 @@
 let $move = document.querySelector('.flier')
 $move.addEventListener('click', () =>{
+    let $creator = document.querySelector('.creator')
+    $creator.classList.toggle('move')
     let $footer = document.querySelector('.footer')
-    $footer.classList.toggle('move')
+    $footer.classList.toggle('z-index-3')
 })

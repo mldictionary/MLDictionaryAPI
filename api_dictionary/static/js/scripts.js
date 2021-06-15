7d3df6d0167c365c5c52b6b938e831fbe1353baa
utrape <pablosud025@gmail.com>
2021-06-15 13:52:01 -0300

    Add event on creator image

--- api_dictionary/static/js/scripts.js
+++ api_dictionary/static/js/scripts.js
@@ -5,3 +5,8 @@ $move.addEventListener('click', () =>{
     let $footer = document.querySelector('.footer')
     $footer.classList.toggle('z-index-3')
 })
+
+let $image = document.querySelector('.image')
+$image.addEventListener('click', ()=>{
+    window.open('https://www.linkedin.com/in/pabloemidio/', target='_blank')
+})

78cbc52d2ca9a3623ef26026710f9ba3154aabe0
utrape <pablosud025@gmail.com>
2021-06-24 14:06:42 -0300

    Update to 'ml'dictionary new rules

--- api_dictionary/ext/webscrapy/translator.py
+++ api_dictionary/ext/webscrapy/translator.py
@@ -1,30 +1,7 @@
-import re
-from typing import Union, List
-
-from .dictionary import Dictionary
+from mldictionary import Dictionary
 
 class Translator(Dictionary):
     URL = 'https://www.linguee.com/english-portuguese/search?source=auto&query={}'
-    XPATH = '//span[@class="tag_trans"]'
-    
-    def __repr__(self) -> str:
-        return 'Translator'
-    
-    
-    def return_meaning(self, word: str) -> Union[List[str], bool]:
-        try:
-            if len(meanings := self._get_meanings(word))>0:
-                def text_formatter(mean: str)->str:
-                    mean = mean.replace('\n    \t                ', '').replace(':', '.')
-                    mean = mean.replace('\n        \n         ', '')
-                    return  re.sub('<[^>]*>', '', mean).split()[0].strip()
-                meanings = list(dict.fromkeys(map(text_formatter, meanings)))
-                howmany = len(meanings) if len(meanings)<=5 else 6
-                return [meanings[i] for i in range(len(meanings)) \
-                            if not len(meanings[i].split())>1 \
-                                if not meanings[i]==word and not '-' in meanings[i]][:howmany]
-            else:
-                return False
-        except Exception as error:
-            print(error)
-            return False
+    LANGUAGE = 'Translator(en-pt)'
+    TARGET_TAG = 'span'
+    TARGET_ATTR = {'class': 'tag_trans'}

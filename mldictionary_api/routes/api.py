402749389dbdbf9607a908aa10a11edcc0ecdab3
Pablo Emidio <p.emidiodev@gmail.com>
2022-01-17 21:38:55 -0300

    Using a class to process response

--- mldictionary_api/routes/api.py
+++ mldictionary_api/routes/api.py
@@ -1,67 +1,61 @@
-import traceback
-
-from flask import Blueprint, jsonify, request
+from flask import Blueprint
 from werkzeug.exceptions import NotFound, InternalServerError, TooManyRequests
 
-from mldictionary_api.models import RedisRequests
-from mldictionary_api.const import (
-    API_PREFIX,
-    DICTIONARIES,
-    LOCAL_ADDR,
-    TOTAL_REQUESTS_ALLOW,
-    TTL_REQUEST,
+from mldictionary_api.const import API_PREFIX
+from mldictionary_api.resources.response import ResponseAPI
+from mldictionary_api.resources.const import (
+    ENGLISH_REPR,
+    ENGLISH_TO_PORTUGUESE_REPR,
+    PORTUGUESE_REPR,
+    PORTUGUESE_TO_ENGLISH,
+    SPANISH_REPR,
 )
-
 
 api = Blueprint('mldictionary_api', __name__, url_prefix=API_PREFIX)
 
 
 @api.route('/dictionary/en/<word>/')
+def english(word: str):
+    return ResponseAPI().get_meanings(ENGLISH_REPR, word)
+
+
 @api.route('/dictionary/pt/<word>/')
+def portuguese(word: str):
+    return ResponseAPI().get_meanings(PORTUGUESE_REPR, word)
+
+
 @api.route('/dictionary/es/<word>/')
+def spanish(word: str):
+    return ResponseAPI().get_meanings(SPANISH_REPR, word)
+
+
 @api.route('/translator/en-pt/<word>/')
+def english_to_portuguese(word: str):
+    return ResponseAPI().get_meanings(ENGLISH_TO_PORTUGUESE_REPR, word)
+
+
 @api.route('/translator/pt-en/<word>/')
-def dictionary(word: str):
-
-    requests_db = RedisRequests()
-
-    choice = request.url.split('/')[5]
-    dictionary = DICTIONARIES[choice]
-    request_ip = request.remote_addr if not request.headers.getlist("X-Forwarded-For") else request.headers.getlist("X-Forwarded-For")[0]
-    total_requests = requests_db.get(f'requests:{request_ip}')
-    if not (meanings := dictionary.get_meanings(word)):
-        raise NotFound(f'"{word}" not found, check the spelling and try again')
-    if request_ip != LOCAL_ADDR:
-        if total_requests > TOTAL_REQUESTS_ALLOW:
-            raise TooManyRequests(
-                f'The address {request_ip} is allow to make only "{TOTAL_REQUESTS_ALLOW}" requests '
-                f'wait until {int(TTL_REQUEST / 60)} minutes and try again'
-            )
-
-    requests_db.set(f'requests:{request_ip}', str(total_requests + 1), TTL_REQUEST)
-
-    return jsonify({'source': dictionary.URL.format(word), 'meanings': meanings}), 200
+def portuguese_to_english(word: str):
+    return ResponseAPI().get_meanings(PORTUGUESE_TO_ENGLISH, word)
 
 
 @api.app_errorhandler(NotFound)
 def not_found(err):
-    traceback.print_tb(err.__traceback__)
-    return jsonify({'message': err.description}), err.code
+    return ResponseAPI().handle_error(err)
 
 
 @api.app_errorhandler(TooManyRequests)
 def too_many_requests(err):
-    traceback.print_tb(err.__traceback__)
-    return jsonify({'message': err.description}), err.code
+    return ResponseAPI().handle_error(err)
 
 
 @api.app_errorhandler(InternalServerError)
 def internal_error(err):
-    traceback.print_tb(err.__traceback__)
-    return jsonify({'message': err.description}), err.code
+    return ResponseAPI().handle_error(err)
 
 
 @api.app_errorhandler(Exception)
 def general_exception(err):
-    traceback.print_tb(err.__traceback__)
-    return jsonify({'message': 'Don\'t recognize erro'}), 500
+    err.description = 'Don\'t recognize erro'
+    err.code = 500
+    return ResponseAPI().handle_error(err)

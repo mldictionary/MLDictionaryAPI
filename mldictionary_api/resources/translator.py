73a8226670d4a4f851c5c2d6b925110551552b7b
Pablo Emídio S.S <p.emidiodev@gmail.com>
2022-03-14 23:18:28 -0300

    Update translator.py

--- mldictionary_api/resources/translator.py
+++ mldictionary_api/resources/translator.py
@@ -2,7 +2,7 @@ from mldictionary import Dictionary
 
 
 class Translator(Dictionary):
-    URL = 'https://www.linguee.com/english-portuguese/search?source=auto&query={}'
+    url = 'https://www.linguee.com/english-portuguese/search?source=auto&query={}'
     language = 'Translator(en-pt)'
     target_tag = 'a'
     target_attr = {'class': 'featured'}
